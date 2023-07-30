d1928f275ecfe6613a5132408e58692f1d37db1f
Skyler Giannetti <sjg5901@psu.edu>
2023-07-30 18:46:52 +0000

    Status codes lab complete

--- src/main/java/JavalinSingleton.java
+++ src/main/java/JavalinSingleton.java
@@ -18,7 +18,7 @@ public class JavalinSingleton {
         app.get("/client-side-err", ctx -> {
         
             //write logic here
-            
+            ctx.status(400);
         });
 
         return app;
